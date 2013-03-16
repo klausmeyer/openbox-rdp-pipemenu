f851368c188f42c63c0042869055d841b0041eb3
Klaus Meyer <spam@klaus-meyer.net>
2013-03-16 22:10:57 +0100

    tools for (de-)crypting passwords with blowfish

---
+++ pwcrypt.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+
+#
+# This file is part of openbox-rdp-pipemenu. openbox-rdp-pipemenu is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright (c) 2013 Klaus Meyer 
+# http://www.klaus-meyer.net/
+#
+
+use strict;
+use 5.010;
+use Config::Simple;
+use Crypt::Blowfish;
+use MIME::Base64;
+
+my %config;
+
+sub blowfish_padding {
+  my $text = shift;
+  my $step = 8;
+  my $mod = length($text) % $step;
+  if($mod != 0) {
+    my $padding = $step - $mod;
+    $text = $text . ("\x00" x $padding);
+  }
+  return $text;
+}
+
+Config::Simple->import_from($ENV{"HOME"} . "/.rdp/config", \%config);
+my $key = blowfish_padding($config{"crypt_key"});
+my $cipher = new Crypt::Blowfish $key;
+
+if($#ARGV + 1 == 1) {
+  my $plain = $ARGV[0];
+  my $crypted = "";
+  # encrypt text using blowfish in 8 byte parts
+  my $offset = 0;
+  my $junk_size = 8;
+  my $junk = "";
+  do {
+    $junk = substr($plain, $offset, $junk_size);
+    if(length($junk) > 0) {
+      $crypted .= $cipher->encrypt(blowfish_padding($junk));
+      $offset += $junk_size;
+    }
+  }
+  until(length($junk) < $junk_size);
+  # encode crypted text in base64
+  $crypted = encode_base64($crypted);
+  say $crypted;
+}
+
+exit 0;
